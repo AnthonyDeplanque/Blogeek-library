a29f585d772b3f6cc398e6049c63399fb3a09766
anthony Deplanque <anthony.deplanque3184@gmail.com>
2021-11-12 23:13:48 +0100

    feat(model): added date_of_write and creator in comments

--- models/interactions/comments.ts
+++ models/interactions/comments.ts
@@ -1,6 +1,10 @@
+import { Users } from "../Users";
+
 export interface Comments {
   id: string;
   id_article: string;
   id_user: string;
   content: string;
+  date_of_write: number;
+  creator?: Users[];
 }
