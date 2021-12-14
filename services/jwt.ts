c5c20da24764878e2e4211dad1c2e0b31ff024a9
Seteemio <68556559+seteemio@users.noreply.github.com>
2021-12-14 09:01:22 +0100

    fix(jwt):ts ignore for frontend

--- services/jwt.ts
+++ services/jwt.ts
@@ -1,26 +1,27 @@
+//@ts-ignore
 const JWT = require('jsonwebtoken');
 const expressJWT = require("express-jwt");
 
 const createToken = (payload: string) => {
-  const dateNowForToken: number = Date.now() / 1000;
-  const expiration: number = Math.floor(dateNowForToken + 60 * 60 * 24);
-  const token = JWT.sign(
-    {
-      data: payload,
-      exp: expiration
-    },
-    process.env.JWT_KEY
-  );
-  return token;
+    const dateNowForToken: number = Date.now() / 1000;
+    const expiration: number = Math.floor(dateNowForToken + 60 * 60 * 24);
+    const token = JWT.sign(
+        {
+            data: payload,
+            exp: expiration
+        },
+        process.env.JWT_KEY
+    );
+    return token;
 }
 
 const decodeToken = (token: any) => {
-  return JWT.decode(token);
+    return JWT.decode(token);
 }
 
 const authWithJWT = expressJWT({
-  secret: process.env.JWT_KEY,
-  algorithms: ["HS256"]
+    secret: process.env.JWT_KEY,
+    algorithms: ["HS256"]
 });
 
 module.exports = { createToken, decodeToken, authWithJWT };
